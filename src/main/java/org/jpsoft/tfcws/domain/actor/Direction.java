e3eaedc099ea73a38226b199ced183d8e386aace
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2025-10-26 20:19:41 +0100

    Add additional cardinal directions to Direction enum

--- src/main/java/org/jpsoft/tfcws/domain/actor/Direction.java
+++ src/main/java/org/jpsoft/tfcws/domain/actor/Direction.java
@@ -4,14 +4,19 @@ package org.jpsoft.tfcws.domain.actor;
  * Direcciones cardinales usadas para orientar actores y movimientos en el mundo.
  *
  * <p>Este {@code enum} representa las cuatro direcciones básicas que pueden
- * adoptar entidades o estructuras dentro del juego: norte, este, sur y oeste.</p>
+ * adoptar entidades o estructuras dentro del juego: NORTH, NORTH_EAST, NORTH_WEST, EAST, WEST,
+ * SOUTH_EAST, SOUTH_WEST, SOUTH.</p>
  *
  * @author Jose Luis García Pelayo
  * @since 1.0
  */
 public enum Direction {
     NORTH,
+    NORTH_EAST,
     EAST,
+    SOUTH_EAST,
     SOUTH,
+    SOUTH_WEST,
+    NORTH_WEST,
     WEST
 }
