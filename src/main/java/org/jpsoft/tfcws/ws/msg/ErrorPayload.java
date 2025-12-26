e45624b29a8d5d34dcf47c5ab27d60d47d53f51e
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2025-12-26 20:32:10 +0100

    Add ErrorPayload class to encapsulate error response details

---
+++ src/main/java/org/jpsoft/tfcws/ws/msg/ErrorPayload.java
@@ -0,0 +1,15 @@
+package org.jpsoft.tfcws.ws.msg;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorPayload {
+    private String code;
+    private String message;
+}
