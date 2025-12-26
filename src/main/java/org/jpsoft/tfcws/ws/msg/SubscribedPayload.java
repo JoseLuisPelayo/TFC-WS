00b316abd8c0e96fb0ccc034fdd8b2074c6f9dc5
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2025-12-26 21:38:05 +0100

    Add JoinPayload class and rename SnapShotZonePayload to SubscribedPayload

---
+++ src/main/java/org/jpsoft/tfcws/ws/msg/SubscribedPayload.java
@@ -0,0 +1,18 @@
+package org.jpsoft.tfcws.ws.msg;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubscribedPayload {
+    private String zoneId;
+    private List<PlayerViewPayload> players;
+}
